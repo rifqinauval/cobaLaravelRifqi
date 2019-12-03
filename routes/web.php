e9cc370411f55f67d453b1c1db3eb4e7a9c76fd2
rifqinauval <rifqinauval11@gmail.com>
2019-12-03 18:39:49 +0700

    selesai video 1 laravel

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    $nama = "Rifqi Nauval";
+    return view('about', ['nama' => $nama]);
 });
